3154b0fefa68b044a2428e0f409a8d3bbc673c5d
Ignacio Fonts <fontsignacio@gmail.com>
2022-12-01 12:56:24 -0300

    Update Stepper

--- flutter_stepper/lib/main.dart
+++ flutter_stepper/lib/main.dart
@@ -1,52 +1,64 @@
 import 'package:flutter/material.dart';
 void main(){
-  runApp(new MaterialApp(
+  runApp(const MaterialApp(
     home: MyStepper(),
   ));
 }
 
 class MyStepper extends StatefulWidget {
+  const MyStepper({super.key});
+
   @override
-  _MyStepperState createState() => _MyStepperState();
+  State<MyStepper> createState() => _MyStepperState();
 }
 
 class _MyStepperState extends State<MyStepper> {
   int _currentStep = 0;
-
+  
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text ("Demo Stepper"),
+    return Scaffold(
+      appBar: AppBar(
+        title:const Text ("Steper"),
+        backgroundColor: Colors.blueGrey,
       ),
-      body: new Container(
-          child: new Stepper(
-            currentStep: this._currentStep,
-            steps: [
-              new Step(title: new Text ("Paso 1"), content: new Text ("Aprender Flutter")),
-              new Step(title: new Text ("Paso 2"), content: new Text ("Desarrollar Aplicacion")),
-              new Step(title: new Text ("Paso 3"), content: new Text ("Publicar Aplicacion"))
-            ],
-            type: StepperType.vertical,
-            onStepCancel: () {
-              setState(() {
-                if (_currentStep > 0)_currentStep--;
-                else _currentStep = 0;
-              });
-            },
-            onStepTapped: (Step) {
-              setState(() {
-                _currentStep= Step;
-              });
-            },
-            onStepContinue: () {
-              setState(() {
-                if (_currentStep < 2) _currentStep++;
-                else _currentStep = 0;
-              });
-            },
-          ),        
-        )
-      );
+      body: Stepper(
+        // ignore: unnecessary_this
+        currentStep: this._currentStep,
+        // ignore: prefer_const_literals_to_create_immutables
+        steps: [
+          const Step(title:  Text ("Step 1"), content:  Text ("Learn Flutter")),
+          const Step(title:  Text ("Step 2"), content:  Text ("Develop Apps")),
+          const Step(title:  Text ("Step 3"), content:  Text ("Post App"))
+        ],
+        type: StepperType.vertical,
+        onStepCancel: () {
+          setState(() {
+            if (_currentStep > 0) {
+              _currentStep--;
+            } 
+            else {
+              _currentStep = 0;
+            }
+          });
+        },
+        // ignore: non_constant_identifier_names, avoid_types_as_parameter_names
+        onStepTapped: (Step) {
+          setState(() {
+            _currentStep= Step;
+          });
+        },
+        onStepContinue: () {
+          setState(() {
+            if (_currentStep < 2) {
+              _currentStep++;
+            } 
+            else{
+              _currentStep = 0;  
+            } 
+          });
+        },
+      )
+    );
   }
 }
